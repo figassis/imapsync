6074da9081c84f7f52d46cd4c363cd5e1f205790
Assis Ngolo <figassis@nellcorp.com>
2016-12-04 20:14:28 +0100

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+if [ $# -ne 3 ]; then
+    echo $0: usage: setup.sh source_ip destination_ip users_file
+    exit 1
+fi
+
+source=$1
+destination=$1
+users=$3
+
 #apt-get -y install libio-socket-ssl-perl
-tools/migrateIMAP.pl -S source_ip:993 -D destination_ip:993 -i users.txt
+tools/migrateIMAP.pl -S $source:993 -D $destination:993 -i $users
